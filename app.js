44ce2669afc8521d9fb82611f6e3ea7a84ed9dda
ArthurAsGon <webbmor2@gmail.com>
2024-02-22 16:34:37 -0300

    commit aula 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{
+    res.send("Teste")
+})
+
+app.listen(port, () =>{
+    console.log('variavel'+ port)
+    console.log('LIsten on ${port}')
+})
---
+++ index.js
@@ -0,0 +1,27 @@
+function ola()
+{
+    console.log("Olá");
+}
+
+const saudacao = () =>
+{
+    var data = new Date()
+    return data.getHours() < 12? "Bom dia" : data.getHours() < 18? "Boa tarde": "Boa noite";
+}
+
+
+ola();
+console.log("A saudação é : " + saudacao())
+
+
+console.log("Snippet correto")
+
+console.log('0')
+
+function teste(){
+var data = new Date()
+return data.getHours()
+}
+teste();
+
+
